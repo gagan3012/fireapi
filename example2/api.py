799bc69de04893a8a290684b72a3999ff2dcf0c7
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-08 18:21:35 -0700

    Create api.py

---
+++ example2/api.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+
+model = tf.keras.models.load_model('model.h5')
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+
+
+def input_to_model(sentence: list):
+    sequences = tokenizer.texts_to_sequences(sentence)
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    result = model.predict(padded)
+
+    return {'data': result}
