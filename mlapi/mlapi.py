6dc774492f1e950717b016351bdbd18f3b450212
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-11 15:35:47 -0700

    Update mlapi.py

--- mlapi/mlapi.py
+++ mlapi/mlapi.py
@@ -1,4 +1,3 @@
-
 from fastapi import Depends, FastAPI, File, UploadFile, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from starlette.middleware.cors import CORSMiddleware
