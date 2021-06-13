448493dd566648b6d88ac582e315c211515affa4
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-13 12:22:20 -0700

    Update mlapi.py

--- mlapi/mlapi.py
+++ mlapi/mlapi.py
@@ -51,6 +51,7 @@ async def startup_event():
         'username': os.environ['BUDGET_USERNAME'],
         'password': os.environ['BUDGET_PWD'],
     }
+
 @app.get("/")
 def health_check():
     return {"message": "Welcome to Fire ML Please visit docs to get started"}
