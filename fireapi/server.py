2a107bcb9cd50d188be21aee253735b4eb0b990c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-17 09:56:54 -0700

    Update server.py

--- fireapi/server.py
+++ fireapi/server.py
@@ -98,3 +98,4 @@ async def predict(request: Request,
 @app.post("/predict_image/")
 async def predict_image(request: UploadFile = File(...),
                         _: str = Depends(verify)) -> Response:
+    """
