65962fe4238fc483ec9dfdc894d457ade22790e6
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-17 09:56:26 -0700

    Update server.py

--- fireapi/server.py
+++ fireapi/server.py
@@ -90,3 +90,4 @@ async def predict(request: Request,
         raise HTTPException(
             status_code=500,
             detail="The predictor could not be loaded. Please check the logs "
+                   "for more detail.",
