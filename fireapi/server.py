53ba288959b70b733f3a170ae6f38e3995655d02
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-17 09:57:26 -0700

    Update server.py

--- fireapi/server.py
+++ fireapi/server.py
@@ -110,3 +110,4 @@ async def predict_image(request: UploadFile = File(...),
             detail="The predictor could not be loaded. Please check the logs "
                    "for more detail.",
         )
+    return await PREDICTOR.predict(request)
