730794bc0bdc3762bfde8cc66b8a34efd167160f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:00:02 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -5,3 +5,4 @@ from uuid import uuid4
 def launch_local(self,
                  predictor_class,
                  requirements: Union[Text, List] = None,
+                 dockerfile_path: Text = None,
