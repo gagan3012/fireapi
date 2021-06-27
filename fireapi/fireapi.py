a9499c287d2c9add3ad4c3c759668db4cd332eab
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:15:08 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -45,6 +45,7 @@ def get_nginx_conf_contents(self,
             base_path, 'template-nginx.conf')
 
     with open(nginx_config_path, 'r') as f:
+        nginx_config_content = f.read()
 def launch_local(self,
                  predictor_class,
                  requirements: Union[Text, List] = None,
