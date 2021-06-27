a6901694a82c1b1dcdeabc4e13d55c722f07f140
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:13:40 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -16,6 +16,7 @@ def get_docker_file_contents(self, dockerfile_path: Text):
 
 
 def get_requirements_file_contents(self, requirements_path: Text):
+    if requirements_path is None:
 def launch_local(self,
                  predictor_class,
                  requirements: Union[Text, List] = None,
