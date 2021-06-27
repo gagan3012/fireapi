f67bfe345e1f8e959a5f38bf5177311dc64d77a6
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:00:14 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -9,4 +9,5 @@ def launch_local(self,
                  bucket_name: Text = None,
                  username: Text = 'budget',
                  password: Text = str(uuid4())):
+    """
     Launch API locally at 0.0.0.0:8000 via docker to simulate endpoint
