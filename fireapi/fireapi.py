f0d37b548e1c840dd6462a48a4de97e288374b14
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:00:43 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -21,5 +21,6 @@ def launch_local(self,
     :param bucket_name: name of bucket to store predictor class.
     :return:
     """
+    if bucket_name is None:
         bucket_name = f'budget_bucket_{self.unique_id}'
     create_bucket_if_not_exists(bucket_name)
