a2feaa8fc77da7b019a023942ccbd6dd59e39d2e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:14:18 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -29,6 +29,7 @@ def get_docker_compose_contents(self, docker_compose_path: Text = None):
         base_path = os.path.dirname(os.path.abspath(__file__))
         docker_compose_path = os.path.join(
             base_path, 'template-compose.yaml')
+
 def launch_local(self,
                  predictor_class,
                  requirements: Union[Text, List] = None,
