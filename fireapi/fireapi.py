87511443ff902b8c34f103e17ab856bb128a4395
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-27 09:14:37 -0700

    Update fireapi.py

--- fireapi/fireapi.py
+++ fireapi/fireapi.py
@@ -36,6 +36,7 @@ def get_docker_compose_contents(self, docker_compose_path: Text = None):
 
 
 def get_nginx_conf_contents(self,
+                            domain: Text,
 def launch_local(self,
                  predictor_class,
                  requirements: Union[Text, List] = None,
