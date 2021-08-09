260395e62e7e24873fb25d0fa649c895fa0ffc7b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-09 13:29:16 -0700

    Update utils.py

--- fireapi/aws/utils.py
+++ fireapi/aws/utils.py
@@ -39,3 +39,6 @@ def absoluteFilePaths(directory):
 
     for dirpath, _, filenames in os.walk(directory):
         for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
+
+
