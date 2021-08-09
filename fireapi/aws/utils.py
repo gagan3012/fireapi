a8cbe3dad1997eb825f73071596bfe6d9f4193a3
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-09 13:27:51 -0700

    Update utils.py

--- fireapi/aws/utils.py
+++ fireapi/aws/utils.py
@@ -3,3 +3,7 @@ import _pickle as pickle
 import pandas as pd
 import numpy as np
 from path import Path
+from IPython.display import clear_output
+from datetime import datetime
+
+root = Path(os.path.dirname(os.path.abspath(__file__)))
