242aceae8a8ecdc1d1c5a2d91886fb1f602a9376
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-23 11:25:08 -0700

    Delete utils.py

--- fireapi/aws/utils.py
+++
@@ -1,8 +0,0 @@
-
-username_dictionary = {'Linux': 'ec2-user',
-                       'Ubuntu': 'ubuntu',
-                       'Windows': 'ec2-user'}
-
-spot_instance_pricing = pd.read_csv(pull_root() + '/data/spot_instance_pricing.csv')
-ami_data = pd.read_csv(pull_root() + '/data/ami_data.csv')
-ami_data['username'] = ami_data['image_name'].apply(lambda s: find_username(s))
