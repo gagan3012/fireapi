c7eddc1aa24ea177c49b33a76dff69fea42ba27d
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-23 11:22:54 -0700

    Delete utils.py

--- fireapi/aws/utils.py
+++ fireapi/aws/utils.py
@@ -1,18 +1,3 @@
-import os, ast, boto3, random, string, pprint, glob, re, psutil
-import _pickle as pickle
-import pandas as pd
-import numpy as np
-from path import Path
-from IPython.display import clear_output
-from datetime import datetime
-
-root = Path(os.path.dirname(os.path.abspath(__file__)))
-
-
-def full_pickle(title, data):
-    '''pickles the submited data and titles it'''
-    pikd = open(title + '.pickle', 'wb')
-    pickle.dump(data, pikd)
     pikd.close()
 
 
