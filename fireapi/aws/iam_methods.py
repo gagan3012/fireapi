5707c6d31f87fb9c78e9d69f4a5334810ad20210
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-23 11:21:21 -0700

    Delete iam_methods.py

--- fireapi/aws/iam_methods.py
+++ fireapi/aws/iam_methods.py
@@ -1,9 +1,3 @@
-    elif region is None:
-        assert client is not None
-
-    sg = client.describe_security_groups(Filters=[{'Name': 'group-name', 'Values': [spotid]}])['SecurityGroups'][0]
-    return sg
-
 
 # TODO : Split security group functions into create and retrieve
 
