e7eec9c0cbb6782791251eff05adceeb1664bb27
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-23 11:21:04 -0700

    Delete iam_methods.py

--- fireapi/aws/iam_methods.py
+++ fireapi/aws/iam_methods.py
@@ -1,8 +1,3 @@
-import sys, boto3
-from fireapi.aws import utils
-
-
-def create_key_pair(client, profile, kp_dir=None):
     # Create a key pair on AWS
     keypair = client.create_key_pair(KeyName=profile['key_pair'][0])
 
