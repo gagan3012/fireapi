d4fed4b0ce51f70085b01c794ba5135ccdfefcfe
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-23 11:22:45 -0700

    Delete iam_methods.py

--- fireapi/aws/iam_methods.py
+++ fireapi/aws/iam_methods.py
@@ -1,8 +1,1 @@
-
-        if 'InvalidGroup.Duplicate' in str(e):
-            print('Security group detected, re-using...')
-            sg = retrieve_security_group(spotid, client=client)
-        else:
-            raise e
-
     return sg
