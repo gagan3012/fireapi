dc96b9421f38df65677a091fe1f4d7f7bafb5890
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-27 13:22:29 -0700

    Update api.py

--- mlapi/api.py
+++ mlapi/api.py
@@ -49,3 +49,4 @@ class API(ModelServer):
 
     async def predict_image(self, file: UploadFile = File(...)):
         preprocess_fn = self.data_processor.preprocess_fn
+        postprocess_fn = self.data_processor.postprocess_fn
