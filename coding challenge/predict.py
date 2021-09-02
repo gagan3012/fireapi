53f61b3415c8e5a6b802317f5f5ac672ba306b1c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-02 11:44:19 -0700

    Update predict.py

--- coding challenge/predict.py
+++ coding challenge/predict.py
@@ -2,3 +2,5 @@
 
 # This script is meant to load a serialized model file and return a prediction
 # The model can be in Tensorflow or scikit-learn (choose one)
+# The model format can be any serialized file format that is appropriate for ML framework that you choose (choose one)
+
