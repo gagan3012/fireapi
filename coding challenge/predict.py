cac019bfc89c8c6348ddbdce5de7165ce2693e13
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-02 11:47:13 -0700

    Delete predict.py

--- coding challenge/predict.py
+++ coding challenge/predict.py
@@ -1,6 +1,3 @@
-# Step 1 - ML in Python coding quiz
-
-# This script is meant to load a serialized model file and return a prediction
 # The model can be in Tensorflow or scikit-learn (choose one)
 # The model format can be any serialized file format that is appropriate for ML framework that you choose (choose one)
 
