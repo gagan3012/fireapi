b2fb0b90b7dab9e26a3378ad93f4024f1b0a2f73
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-02 11:48:16 -0700

    Delete writeDockerfile.py

--- coding challenge/writeDockerfile.py
+++ coding challenge/writeDockerfile.py
@@ -1,6 +1,3 @@
-        dockerfile = "FROM PYTHON 3.8" \
-                     "COPY REQUIREMNTS.TXT " \
-                     "RUN PIP INSTALL -R REQUIREMENT.TXT" \
                     "COPY PREDICT.PY ./PREDICT.PY"
                     "RUN PYTHON PREDICT.PY"
         f.write(dockerfile)
