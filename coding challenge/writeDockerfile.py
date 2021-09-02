03949b9bc6feed54223e15dc2da4d02604228842
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-02 11:45:45 -0700

    Update writeDockerfile.py

--- coding challenge/writeDockerfile.py
+++ coding challenge/writeDockerfile.py
@@ -1,3 +1,5 @@
 # Step 2 - Docker coding quiz
 
 # This script is meant to create a Dockerfile for the prediction script completed in step 1 (predict.py).
+
+# Please complete the script below (refer to sections marked "TO DO" for parts to be completed)
