ad899110d3a74ee8291751ab2c7e2a9574e8eab8
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-27 10:18:38 -0700

    Update api.py

--- mlapi/api.py
+++ mlapi/api.py
@@ -33,7 +33,6 @@ class API(ModelServer):
 
         docs_url = kwargs.get("docs_url", "/docs")
         title = kwargs.get("title", "Chitra Model Server 🔥")
-        desc = kwargs.get(
 
         self.app: FastAPI = FastAPI(title=title, description=desc, docs_url=docs_url)
         if not preprocess_conf:
