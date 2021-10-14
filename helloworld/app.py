184dd1d8c494f6105f32143260303f56e9c333ab
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-10-13 21:51:20 -0700

    Update app.py

--- helloworld/app.py
+++ helloworld/app.py
@@ -22,3 +22,7 @@ def index():
 # def create_user():
 #     # This is the JSON body the user sent in their POST request.
 #     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
